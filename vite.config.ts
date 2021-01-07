d7d491d5cc287509c737720147b7c04b93a75a05
chenlongfei <dragonfly920130@outlook.com>
2021-01-07 17:51:31 +0800

    feat: 升级 vite 版本到 2.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,33 @@
-import type { UserConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
-const config: UserConfig = {
-    base: '/',
-    port: parseInt(process.env.VITE_PORT) || 8080,
+export default defineConfig({
     alias: {
-        '/@/': resolve(__dirname, 'src')
+        '/@': resolve(__dirname, 'src')
     },
-    esbuildTarget: 'es2020',
-    cssPreprocessOptions: {
-        sass: {
-            modifyVars: {}
+    plugins: [vue()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: {}
+            }
         }
     },
-    proxy: {
-        '/api': {
-            target: 'http://localhost:8081',
-            changeOrigin: true
+    server: {
+        port: parseInt(process.env.VITE_PORT) || 8080,
+        open: true,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8081',
+                changeOrigin: true
+            }
         }
+    },
+    build: {
+        base: '/'
     },
     optimizeDeps: {
         include: ['ant-design-vue/es/locale/zh_CN', 'ant-design-vue/es/locale/en_US', '@ant-design/icons-vue']
     }
-}
-
-export default config
+})
